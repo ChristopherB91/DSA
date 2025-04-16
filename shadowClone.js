b0278b3748e6320132d4429f19711b3e38c01325
Chris <chrisbrowne91@gmail.com>
2025-04-16 09:07:35 -0400

    added the prompt

--- shadowClone.js
+++ shadowClone.js
@@ -1,3 +1,19 @@
+//                                                                                              PROBLEM
+// 🍥 Naruto: Shadow Clone Multiplication
+// Prompt:
+//  Naruto uses his signature Shadow Clone Jutsu! He creates 2 clones, and each clone creates 2 new clones in the next generation, but only up to a certain depth.
+// Generation 1: Naruto
+
+// Generation 2: 2 clones
+
+// Generation 3: Each of those 2 clones makes 2 more (4)
+
+// etc.
+// Given a number n, representing the number of clone generations, write a recursive function that calculates the total number of clones (including the original Naruto) created.
+// Return the total number of Narutos and clones across all generations.
+
+//                                                                                              SOLUTION
+
 const shadowClone = (n) => {
   if (n === 1) {
     return 1;
