0a75935d7fea4e90ec89a260918dc68a028c6b34
Chris <chrisbrowne91@gmail.com>
2025-05-10 13:02:52 -0400

    Zoro Recursion

--- ZoroLostPath.js
+++ ZoroLostPath.js
@@ -8,29 +8,29 @@
 
 // Because of his terrible direction, Zoro ends up completing every possible sequence of routes.
 
-// How many distinct sequences of moves can Zoro take to reach the stronghold in exactly n units?
+// How many distinct path of moves can Zoro take to reach the stronghold in exactly n units?
 // Write a recursive function that returns this number.
 
-const sequences = (n) => {
+const path = (n) => {
   if (n == 1) return 1;
   if (n == 2) return 2;
 
-  return sequences(n - 1) + sequences(n - 2);
+  return path(n - 1) + path(n - 2);
 };
 
-console.log(sequences(5)); // Should return 8
+console.log(path(5)); // Should return 8
 
 // Next we optimize using memoization
 
-const sequences2 = (n, memo = {}) => {
+const path2 = (n, memo = {}) => {
   if (n == 1) return 1;
   if (n == 2) return 2;
   if (n in memo) {
     return memo[n];
   }
 
-  memo[n] = sequences2(n - 1) + sequences2(n - 2);
+  memo[n] = path2(n - 1) + path2(n - 2);
   return memo[n];
 };
 
-console.log(sequences2(30));
+console.log(path2(30));
